2cf1316c4ddd7403cf9cdc15c9c4e1b1ae16d0ae
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-03 06:41:55 +0600

    about page responsive for each devices

--- src/app/(mainPages)/projects/page.jsx
+++ src/app/(mainPages)/projects/page.jsx
@@ -11,18 +11,18 @@ const FramerImage = motion(Image)
 
 const FeaturesProject = ({ type, title, summary, img, github, link }) => {
   return (
-    <article className="w-full flex items-center justify-center rounded-br-2xl  rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative dark:bg-dark dark:border-light">
-      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-3xl bg-dark dark:bg-light ' />
-      <Link href={link} target='_blank' className="w-1/2 cursor-pointer overflow-hidden rounded-lg">
+    <article className="w-full flex items-center justify-center rounded-br-2xl  rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-3xl bg-dark dark:bg-light rounded-br-3xl xs:right-2  sm:h-[102%] xs:w-full xs:rounded-[1.5rem]' />
+      <Link href={link} target='_blank' className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full">
         <FramerImage src={img} alt='akm syful web dev project' priority sizes="(mix-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw" 
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
         />
       </Link>
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6 ">
-        <span className=" text-primary text-xl font-medium dark:text-primaryDark">{type}</span>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className=" text-primary text-xl font-medium dark:text-primaryDark xs:text-base">{type}</span>
         <Link href={link} target='_blank' className="hover:underline underline-offset-2">
-          <h2 className="my-2 w-full text-left text-4xl  font-bold dark:text-light">{title}</h2>
+          <h2 className="my-2 w-full text-left text-4xl  font-bold dark:text-light sm:text-sm">{title}</h2>
         </Link>
         <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
         <div className="mt-2 flex items-center">
@@ -30,7 +30,7 @@ const FeaturesProject = ({ type, title, summary, img, github, link }) => {
             <GithubIcon />
           </Link>
           <Link href={link} target='_blank'
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base"
           >
             Visit Project
           </Link>
@@ -78,9 +78,9 @@ export default function Page() {
   return (
     <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light '>
       <Layout className='pt-16'>
-        <AnimatedText text="Imagination Trumps Knowledge!" />
+        <AnimatedText text="Imagination Trumps Knowledge!" className="mb-16 lg:!text7xl sm:mb-8 sm:!text-6xl  xs:text4xl"/>
         <div className="mb-16" />
-        <div className='grid grid-cols-12 gap-24 gap-y-32'>
+        <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gapy-24 sm:gap-x-0'>
           <div className='col-span-12'>
             <FeaturesProject
               title="Crypto Screener Application"
--- src/components/Pages/About/Skills.jsx
+++ src/components/Pages/About/Skills.jsx
@@ -5,12 +5,12 @@ import { motion } from 'framer-motion'
 
 const Skill = ({ name, x, y }) => {
     return (
-        <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light px-6 py-3 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light'
+        <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light px-6 py-3 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent dark:xs:bg-transparent xs:text-dark dark:xs:text-light xs:font-bold'
             whileHover={{ scale: 1.05 }}
-            initial={{x:0, y:0}}
-            whileInView={{x:x, y:y, transition:{duration: 1.5}}}
-           
-            viewport={{once: true}}
+            initial={{ x: 0, y: 0 }}
+            whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
+
+            viewport={{ once: true }}
         >
             {name}
         </motion.div>
@@ -21,27 +21,34 @@ const Skill = ({ name, x, y }) => {
 export default function Skills() {
     return (
         <>
-            <h1 className='font-bold text-8xl mt-48 w-full text-center'>Skills </h1>
+            <h1 className='font-bold text-8xl mt-48 w-full text-center md:text-6xl md:mt-32'>Skills </h1>
 
-            <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark'>
-            <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light'
+            <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] 
+            
+            lg:bg-circularLightLg dark:lg:bg-circularDarkLg
+            md:bg-circularLightMd dark:ms:bg-circularDarkMd
+            sm:bg-circularLightSm dark:sm:bg-circularDarkSm
+            xs:bg-circularLightXs dark:xs:bg-circularDarkXs
+            
+            '>
+                <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2'
                     whileHover={{ scale: 1.05 }}
                 >
                     Web
                 </motion.div>
-                <Skill name="CSS" x="-5vw" y="-10vw" />
-                <Skill name="HTML" x="-20vw" y="-12vw" />
-                <Skill name="NodeJS" x="14vw" y="15vw" />
-                <Skill name="ReactJS" x="-11vw" y="-5vw" />
-                <Skill name="NextJS" x="-16vw" y="7vw" />
-                <Skill name="Tailwind CSS" x="26vw" y="2vw" />
-                <Skill name="MaterialUI" x="24vw" y="-14vw" />
-                <Skill name="MongoDB" x="12vw" y="6vw" />
-                <Skill name="Mongoose" x="16vw" y="-6vw" />
-                <Skill name="Firebase" x="-22vw" y="18vw" />
-                <Skill name="ExpressJS" x="-5vw" y="12vw" />
+                <Skill name="CSS" x="-5vw" y="-16vw" />
+                <Skill name="HTML" x="-18vw" y="-20vw" />
+                <Skill name="NodeJS" x="14vw" y="23vw" />
+                <Skill name="ReactJS" x="-24vw" y="-8vw" />
+                <Skill name="NextJS" x="-25vw" y="14vw" />
+                <Skill name="Tailwind CSS" x="26vw" y="1vw" />
+                <Skill name="MaterialUI" x="24vw" y="-20vw" />
+                <Skill name="MongoDB" x="22vw" y="12vw" />
+                <Skill name="Mongoose" x="18vw" y="-11vw" />
+                <Skill name="Firebase" x="-8vw" y="24vw" />
+                <Skill name="ExpressJS" x="-5vw" y="14vw" />
                 <Skill name="Figma" x="4vw" y="-20vw" />
-                <Skill name="Git GitHub" x="-23vw" y="1vw" />
+                <Skill name="Git GitHub" x="-25vw" y="2vw" />
             </div>
 
         </>
