1220f5def3630ef5ce9fbbf9f6ddf78e4b60af8c
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-03 07:24:33 +0600

    complete responsive

--- src/app/(mainPages)/articles/page.js
+++ src/app/(mainPages)/articles/page.js
@@ -40,8 +40,8 @@ const MovingImage = ({ title, img, link }) => {
         style={{ x: x, y: y }}
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
-        ref={imageRef} src={img} alt={title} className="z-10  w-96 h-auto hidden absolute rounded-lg "
-        priority sizes="(mix-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw" 
+        ref={imageRef} src={img} alt={title} className="z-10  w-96 h-auto hidden absolute rounded-lg sm:hidden"
+        priority sizes="(mix-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw " 
       />
     </Link>
 
@@ -58,9 +58,9 @@ const Article = ({ img, title, date, link }) => {
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
       viewport={{ once: true }}
-      className="relative w-full p-4 py-6 my-4 rounded-xl flex justify-between items-center bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark">
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex justify-between items-center bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark sm:flex-col">
       <MovingImage title={title} img={img} link={link}  />
-      <span className="text-primary font-semibold pl-4 dark:text-primaryDark">{date}</span>
+      <span className="text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm">{date}</span>
     </motion.li>
   )
 }
@@ -79,7 +79,7 @@ const FeaturedArticles = ({ image, title, time, summary, link }) => {
         />
       </Link>
       <Link href={link} target="_blank">
-        <h2 className="capitalize text-2xl font-bold my-2  mt-2 hover:underline">{title}</h2>
+        <h2 className="capitalize text-2xl font-bold my-2  mt-2 hover:underline xs:text-lg">{title}</h2>
       </Link>
       <p className="text-sm mb-2">{summary}</p>
       <span className="text-primary font-semibold">{time}</span>
@@ -92,8 +92,8 @@ export default function page() {
   return (
     <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light">
       <Layout className="pt-16">
-        <AnimatedText text="Words Can Change The World! " className="mb-16" />
-        <ul className="grid grid-cols-2 gap-16">
+        <AnimatedText text="Words Can Change The World! " className="mb-16 lg:!text7xl sm:mb-8 sm:!text-6xl  xs:text-4xl" />
+        <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
           <FeaturedArticles
             title="Build A Custom Pagination Component In Reactjs From Scratch"
             summary="Learn how to build a custom pagination component in ReactJS from scratch. 
--- src/app/(mainPages)/projects/page.jsx
+++ src/app/(mainPages)/projects/page.jsx
@@ -24,7 +24,7 @@ const FeaturesProject = ({ type, title, summary, img, github, link }) => {
         <Link href={link} target='_blank' className="hover:underline underline-offset-2">
           <h2 className="my-2 w-full text-left text-4xl  font-bold dark:text-light sm:text-sm">{title}</h2>
         </Link>
-        <p className="my-2 font-medium text-dark dark:text-light">{summary}</p>
+        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">{summary}</p>
         <div className="mt-2 flex items-center">
           <Link href={github} target='_blank' className="w-10">
             <GithubIcon />
@@ -43,8 +43,10 @@ const FeaturesProject = ({ type, title, summary, img, github, link }) => {
 
 const Project = ({ type, title, img, github, link }) => {
   return (
-    <article className="w-full flex flex-col items-center justify-between rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light  ">
-      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-3xl bg-dark dark:border-light dark:bg-light' />
+    <article className="w-full flex flex-col items-center justify-between rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light  xs:p-4">
+
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-3xl bg-dark dark:border-light dark:bg-light md:right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]' />
+
       <Link href={link} target='_blank' className="w-full cursor-pointer overflow-hidden rounded-lg">
         <FramerImage src={img} alt='akm syful web dev project' 
         priority sizes="(mix-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw" 
@@ -53,18 +55,18 @@ const Project = ({ type, title, img, github, link }) => {
         />
       </Link>
       <div className="w-fullflex flex-col items-start justify-between mt-4 ">
-        <span className=" text-primary text-xl font-medium dark:text-primaryDark">{type}</span>
+        <span className=" text-primary text-xl font-medium dark:text-primaryDark lg:text-lg md:text-base">{type}</span>
         <Link href={link} target='_blank' className="hover:underline underline-offset-2">
-          <h2 className="my-2 w-full text-left text-3xl  font-bold">{title}</h2>
+          <h2 className="my-2 w-full text-left text-3xl  font-bold lg:text-2xl">{title}</h2>
         </Link>
 
         <div className="mt-2 flex items-center justify-between">
           <Link href={link} target='_blank'
-            className=" text-lg font-semibold underline"
+            className=" text-lg font-semibold underline md:text-base"
           >
             Visit
           </Link>
-          <Link href={github} target='_blank' className="w-8">
+          <Link href={github} target='_blank' className="w-8 md:w-6">
             <GithubIcon />
           </Link>
 
@@ -78,7 +80,7 @@ export default function Page() {
   return (
     <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light '>
       <Layout className='pt-16'>
-        <AnimatedText text="Imagination Trumps Knowledge!" className="mb-16 lg:!text7xl sm:mb-8 sm:!text-6xl  xs:text4xl"/>
+        <AnimatedText text="Imagination Trumps Knowledge!" className="mb-16 lg:!text7xl sm:mb-8 sm:!text-6xl  xs:text-4xl"/>
         <div className="mb-16" />
         <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gapy-24 sm:gap-x-0'>
           <div className='col-span-12'>
@@ -91,7 +93,7 @@ export default function Page() {
               type="Featured Project"
             />
           </div>
-          <div className='col-span-6'>
+          <div className='col-span-6 sm:col-span-12'>
             <Project
               title="Crypto Screener Application"
               img={project1}
@@ -100,7 +102,7 @@ export default function Page() {
               type="Featured Project"
             />
           </div>
-          <div className='col-span-6'>
+          <div className='col-span-6 sm:col-span-12'>
             <Project
               title="Crypto Screener Application"
               img={project1}
@@ -119,7 +121,7 @@ export default function Page() {
               type="Featured Project"
             />
           </div>
-          <div className='col-span-6'>
+          <div className='col-span-6 sm:col-span-12'>
             <Project
               title="Crypto Screener Application"
               img={project1}
@@ -128,7 +130,7 @@ export default function Page() {
               type="Featured Project"
             />
           </div>
-          <div className='col-span-6'>
+          <div className='col-span-6 sm:col-span-12'>
             <Project
               title="Crypto Screener Application"
               img={project1}
